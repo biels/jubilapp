cab07d2940882e525fd1f27c37d818be12988b06
Biel <bielsimon2@gmail.com>
2018-12-14 21:16:17 +0100

    Progress in calendar.service.ts

--- src/profile/calendar.service.ts
+++ src/profile/calendar.service.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import * as ical from 'ical-generator'
 import { User } from '../model/user/user.entity';
 import { EventAttendee } from '../model/event-attendee/event-attendee.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CalendarService {
@@ -11,11 +12,16 @@ export class CalendarService {
   constructor(
     @InjectRepository(UserRepository)
     private readonly userRepository: UserRepository,
+    @InjectRepository(EventAttendee)
+    private readonly eventAttendeeRepository: Repository<EventAttendee>
   ) {
   }
-
+  async getUserAttendingList(user: User) {
+    const eventAttendeList = await this.eventAttendeeRepository.find({ where: { user: user }, relations: ['event'] });
+    return eventAttendeList.filter(ea => ea.attending)
+  }
   async generateCalendarForUser(user: User){
-    const getUserWithEvents = this.userRepository.findOneOrFail(user.id, {relations: ['attendingEvents', 'user.attendingEvents']});
+    const attendingList = await this.getUserAttendingList(user);
     const mapEventToICalEvent = (event: EventAttendee) => {};
     // const events: ical.EventData[] = user.attendingEvents.map(mapEventToICalEvent)
     // const iCalCalendar = ical({
