f1c9c78ac4118de08d9f16169f40e418fe4cc870
Biel Simon Rojas <bielsimon2@gmail.com>
2019-01-05 22:02:16 +0100

    Improved calendar.service.ts

--- src/profile/calendar.service.ts
+++ src/profile/calendar.service.ts
@@ -7,6 +7,7 @@ import { EventAttendee } from '../model/event-attendee/event-attendee.entity';
 import { Repository } from 'typeorm';
 import { Event } from '../model/event/event.entity';
 import * as moment from 'moment';
+import { attendeeStatus } from 'ical-generator';
 
 @Injectable()
 export class CalendarService {
@@ -22,7 +23,7 @@ export class CalendarService {
   async getUserAttendingList(user: User) {
     const eventAttendeList = await this.eventAttendeeRepository.find({
       where: { user: user },
-      relations: ['event', 'event.user'],
+      relations: ['event', 'event.user', 'event.eventAttendees', 'event.eventAttendees.user'],
     });
     return eventAttendeList.filter(ea => ea.attending);
   }
@@ -32,21 +33,34 @@ export class CalendarService {
     const mapEventToICalEvent = (e: Event): ical.EventData => {
       // console.log(`Event details: `, e);
       return {
-        start: moment(e.startDate || new Date()),
-        end: moment(e.endDate || new Date()).add(1, 'hour'),
-        summary: e.name,
+        start: e.startDate,
+        end: e.endDate || moment(e.startDate).add(1, 'hour'),
+        summary: e.name || e.description,
         description: e.description,
-        organizer: `${e.user.name} ${e.user.surname} <${e.user.email}>`,
+        organizer: `${e.user.name || ''} ${e.user.surname || ''} <${e.user.email}>`,
         location: (e.latitude && e.longitude) ? `${e.latitude} ${e.longitude}` : null,
+        attendees: e.eventAttendees
+          .map(at => {
+            let user = at.user
+            const status: attendeeStatus = at.attending ? 'ACCEPTED' : 'DECLINED';
+            return {
+              name: `${user.name || ''} ${user.surname || ''}`,
+              email: user.email,
+              status: status
+            }
+          }),
       };
     };
     // TODO Implement mapping
     const calendarEvents: ical.EventData[] = attendingList
       .filter(ea => ea.attending)
       .map(ea => ea.event)
+      .filter(e => e.startDate != null)
       .map(mapEventToICalEvent);
     const iCalCalendar = ical({
-      name: `${user.name}'s calendar`,
+      name: `Jubilapp ${user.name}'s calendar`,
+      domain: 'jubilapp.com',
+      prodId: '//ironman-industries.com//ical-generator//EN',
       events: calendarEvents,
     });
     return iCalCalendar.toString();
